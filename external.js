6636a6fc4e62b46fc85181d65efd65e84eac6a80
Gokul S <gokul270520@gmail.com>
2022-03-22 16:39:32 +0530

    external js dom

---
+++ external.js
@@ -0,0 +1,5 @@
+document.querySelector("h1").innerHTML="About Gokul s";
+
+document.getElementsByClassName("content")[0].classList.add("para");
+document.getElementsByClassName("content")[1].classList.add("para");
+document.getElementsByClassName("content")[2].classList.add("para");
